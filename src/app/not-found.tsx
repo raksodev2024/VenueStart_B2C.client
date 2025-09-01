81e02f130a646266bcccc896b01f45603a39c64e
jimuel20 <104748076+jimuel20@users.noreply.github.com>
2025-09-01 13:18:03 +0800

    Page Not Found design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,20 @@
-import Link from 'next/link'
+import Link from "next/link";
+import styles from "./not-found.module.css";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className={`${styles.notfoundwrapper} container p-5`}>
+      <h1 className={`${styles.notfoundtitle}`}>404</h1>
+      <h2>OOPS! Page Not Found</h2>
+      <p>
+        The page you’re looking for doesn’t exist or may have been moved. Please
+        contact the system administrator if you believe this is an error.
+      </p>
+      <Link href="/">
+        <button className="btn btn-custom rounded-0 px-4 py-2 fw-bold">
+          Return Home
+        </button>
+      </Link>
     </div>
-  )
+  );
 }
